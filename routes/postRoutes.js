8eccd5d919cf9deb4b34b880922fdb7a40f3f47c
Variya Parth <63655701+Parthvariya2908@users.noreply.github.com>
2024-08-06 17:36:25 +0530

    Update postRoutes.js

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { authenticateToken, checkTFA } = require("../middleware/authentication");
+const { authenticateToken } = require("../middleware/authentication");
 const { upload } = require("../middleware/authentication");
 
 const postController = require("../controllers/postController");
@@ -8,11 +8,9 @@ const postController = require("../controllers/postController");
 router.post(
   "/posts",
   authenticateToken,
-  checkTFA,
-  upload.single("photo"),
   postController.createPost
 );
-router.get("/posts", authenticateToken, checkTFA, postController.getPosts);
+router.get("/posts", authenticateToken, postController.getPosts);
 router.get("/allposts", postController.getAllPosts);
 
 module.exports = router;
